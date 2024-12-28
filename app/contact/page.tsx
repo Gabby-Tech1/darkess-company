704a783458de7cadaaedbf5b6fb60a87a8c786e5
Derryblinks <db723252@gmail.com>
2024-12-28 00:28:26 +0000

    Complete Contact page and the Services page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import React from 'react';
+import Hero from '@/public/Contact.png'
+import Image from 'next/image'
 
-const page = () => {
+
+
+const ContactPage = () => {
   return (
-    <div>Contact Page under construction</div>
-  )
-}
+    <section>
+      {/* Background Section */}
+      <div className="md:h-[60vh] h-[50vw] flex items-center justify-center w-screen">
+            <Image src={Hero} alt="hero section for about page" className="bg-cover bg-center relative w-full h-full" />
+            <div className="absolute">
+                <div className="text-white text-center flex ">
+                  {/* <h1 className="text-4xl font-bold">Drakes Drilling and Construction Limited</h1> */}
+                    {/* <p className="text-2xl mt-2">Contact Us</p> */}
+                    <h1 className="text-4xl md:text-5xl font-bold">
+                        Contact Us
+                    </h1>
+                </div>
+            </div>
+        </div>
 
-export default page
+      {/* Contact Info Section */}
+      <div className="bg-white py-12 px-6 sm:px-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
+          <div className="bg-red-600 text-white p-6 rounded-md flex flex-col items-center !important">
+            <p className="text-xl font-bold">PHONE</p>
+            <p className="mt-2">+233 55 302 4319</p>
+            <p>+233 24 295 9650</p>
+          </div>
+          <div className="bg-red-600 text-white p-6 rounded-md flex flex-col items-center">
+            <p className="text-xl font-bold">PHONE</p>
+            <p className="mt-2">+233 55 302 4319</p>
+            <p>+233 24 295 9650</p>
+          </div>
+          <div className="bg-red-600 text-white p-6 rounded-md flex flex-col items-center">
+            <p className="text-xl font-bold">LOCATION</p>
+            <p className="mt-2 text-center">Sapleiman Adjacent to VKAY Lodge</p>
+          </div>
+          <div className="bg-red-600 text-white p-6 rounded-md flex flex-col items-center">
+            <p className="text-xl font-bold">EMAIL</p>
+            <p className="mt-2">yakubur259@gmail.com</p>
+          </div>
+          <div className="bg-red-600 text-white p-6 rounded-md flex flex-col items-center">
+            <p className="text-xl font-bold">WORKING HOURS</p>
+            <p className="mt-2">Mon-Fri: 08am-09pm</p>
+            <p>Saturday: 1pm-10pm</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Form Section */}
+      <div className="bg-gray-50 py-12 px-6 sm:px-12">
+        <div className="max-w-4xl mx-auto">
+          <h2 className="text-center text-2xl font-bold mb-6">
+            Get in touch with us today to discuss your project needs
+          </h2>
+          <form className="grid grid-cols-1 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+              <input
+                type="text"
+                placeholder="Name"
+                className="border border-gray-300 p-3 rounded-md w-full"
+              />
+              <input
+                type="email"
+                placeholder="Email Address"
+                className="border border-gray-300 p-3 rounded-md w-full"
+              />
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+              <input
+                type="text"
+                placeholder="Phone"
+                className="border border-gray-300 p-3 rounded-md w-full"
+              />
+              <input
+                type="text"
+                placeholder="Subject"
+                className="border border-gray-300 p-3 rounded-md w-full"
+              />
+            </div>
+            <textarea
+              placeholder="Message"
+              rows= {5}
+              className="border border-gray-300 p-3 rounded-md w-full"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-blue-600 text-white py-3 px-6 rounded-md font-bold hover:bg-blue-700"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+
+      {/* Footer Note */}
+      <div className="bg-white py-6">
+        <p className="text-center text-gray-600">
+          Please don’t hesitate to contact us with any questions, enquiries or project proposals.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default ContactPage;
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,9 +1,198 @@
 import React from 'react'
+import Hero from '@/public/Service_hero.png'
+import Founder from '@/public/Bottom.png'
+import Founder1 from '@/public/Image1.png'
+import Founder2 from '@/public/Image2.png'
+import Founder3 from '@/public/Image3.png'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <div>Service page under construction</div>
-  )
+    <div>
+      <div className="md:h-[60vh] h-[50vw] flex items-center justify-center w-screen">
+        <Image
+          src={Hero}
+          alt="hero section for about page"
+          className="bg-cover bg-center relative w-full h-full"
+        />
+        <div className="absolute">
+          <div className="text-white text-center flex ">
+            <h1 className="text-4xl md:text-xl font-bold">
+              At Dakess Drilling and Construction Limited, we offer a
+              comprehensive range of services to meet your infrastructure needs.
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="py-10 flex flex-col gap-10">
+        <div className="py-10 flex flex-col items-center">
+          <h1 className="text-2xl">We offer the following services</h1>
+          {/* <p className='text-blue text-3xl text-center md:text-4xl '>Dakess Drilling And Construction Limited</p>
+                <p className='text-center lg:px-44 md:px-10 px-4'>At Dakess Drilling and Construction Limited, 
+                  we are a leading provider of expert drilling and construction services, dedicated to 
+                  delivering high-quality solutions to our clients. With a strong commitment to safety, 
+                  innovation, and customer satisfaction, we have established ourselves as a trusted partner
+                   in the industry.</p> */}
+        </div>
+        <div className="flex items-center flex-col-reverse md:flex-row justify-center gap-10 lg:gap-20 lg:mx-44 px-4">
+          <Image
+            src={Founder1}
+            alt="picture of the founder"
+            width={300}
+            height={450}
+          />
+          <div className="flex flex-col gap-4">
+            <h1 className="text-3xl text-blue font-semibold">
+              Drilling and Construction Services
+            </h1>
+            <p className="text-lg">- Borehole drilling for water supply</p>
+            <p className="text-lg">
+              - Foundation drilling for construction projects
+            </p>
+            <p className="text-lg">- Construction management</p>
+            <p className="text-lg">- Site investigation and assessment</p>
+          </div>
+        </div>
+        <div className="py-10 flex items-center flex-col-reverse md:flex-row justify-center gap-10 lg:gap-20 lg:mx-44 px-4">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-3xl text-blue font-semibold">
+              Borehole Installation
+            </h1>
+            <p className="text-lg">- Design and installation of boreholes</p>
+            <p className="text-lg">- Borehole rehabilitation and maintenance</p>
+            <p className="text-lg">- Water yield testing and analysis</p>
+          </div>
+          <Image
+            src={Founder2}
+            alt="picture of the founder"
+            width={300}
+            height={450}
+          />
+        </div>
+
+        <div className="py-10 px-10 flex items-center flex-col-reverse md:flex-row justify-center gap-10 lg:gap-20 lg:mx-44 px-4">
+          <Image
+            src={Founder3}
+            alt="picture of the founder"
+            width={300}
+            height={450}
+          />
+          <div className="flex flex-col gap-4">
+            <h1 className="text-3xl text-blue font-semibold">
+              Water Treatment Solutions
+            </h1>
+            <p className="text-lg">- Water purification systems</p>
+            <p className="text-lg">- Water filtration systems</p>
+            <p className="text-lg">
+              - Water treatment plant design and installation
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+
+      {/* Geological Survey */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
+          <div className="bg-gray-50 shadow-lg rounded-lg overflow-hidden">
+            <div className="p-6">
+              <h3 className="text-lg font-bold text-blue-600">Geological Survey</h3>
+              <ul className="mt-4 text-gray-700 space-y-2">
+                <li>- Site investigation and geological mapping</li>
+                <li>- Soil and rock sampling and analysis</li>
+                <li>- Hydrogeological studies</li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Recreational Services */}
+          <div className="bg-gray-50 shadow-lg rounded-lg overflow-hidden">
+            <div className="p-6">
+              <h3 className="text-lg font-bold text-blue-600">Recreational Services</h3>
+              <ul className="mt-4 text-gray-700 space-y-2">
+                <li>- Swimming Pool construction</li>
+                <li>- Pool maintenance and repair</li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Security Services */}
+          <div className="bg-gray-50 shadow-lg rounded-lg overflow-hidden">
+            <div className="p-6">
+              <h3 className="text-lg font-bold text-blue-600">Security Services</h3>
+              <ul className="mt-4 text-gray-700 space-y-2">
+                <li>- CCTV installation</li>
+                <li>- Electrical fencing</li>
+              </ul>
+            </div>
+          </div>
+          {/* Security Services */}
+          <div className="bg-gray-50 shadow-lg rounded-lg overflow-hidden">
+            <div className="p-6">
+              <h3 className="text-lg font-bold text-blue-600">Electrical Services</h3>
+              <ul className="mt-4 text-gray-700 space-y-2">
+                <li>- Electrical wiring and installation</li>
+                <li>- Electrical maintenance and repair</li>
+              </ul>
+            </div>
+          </div>
+          {/* Security Services */}
+          <div className="bg-gray-50 shadow-lg rounded-lg overflow-hidden">
+            <div className="p-6">
+              <h3 className="text-lg font-bold text-blue-600">Building Finishing Services</h3>
+              <ul className="mt-4 text-gray-700 space-y-2">
+                <li>- POP (Plaster of Paris) construction</li>
+                <li>- Ceiling design and installation</li>
+              </ul>
+            </div>
+          </div>
+          {/* Security Services */}
+          <div className="bg-gray-50 shadow-lg rounded-lg overflow-hidden">
+            <div className="p-6">
+              <h3 className="text-lg font-bold text-blue-600">Industrial Services</h3>
+              <ul className="mt-4 text-gray-700 space-y-2">
+                <li>- Pure Water Factory setup and equipment installation</li>
+                <li>- Bottling plant design and construction</li>
+              </ul>
+            </div>
+          </div>
+          </div>
+        
+
+
+     
+     
+      {/* buttonImage */}
+
+      <div className="md:h-[60vh] h-[50vw] flex items-center justify-center w-screen">
+        <Image
+          src={Founder}
+          alt="hero section for about page"
+          className="bg-cover bg-center relative w-full h-full"
+        />
+        <div className="absolute">
+          <div className="text-white text-center flex ">
+            {/* <h1 className="text-4xl md:text-xl font-bold">
+                    At Dakess Drilling and Construction Limited, we offer a comprehensive
+                     range of services to meet your infrastructure needs.
+
+                    </h1> */}
+            <div className="text-center max-w-4xl mx-auto">
+              <h2 className="text-3xl font-bold mb-6">Why Choose Dakess?</h2>
+              <ul className="list-disc list-inside text-lg space-y-4">
+                <li>
+                  Experienced professionals with decades of combined experience
+                </li>
+                <li>State-of-the-art equipment</li>
+                <li>Customer-centric solutions</li>
+                <li>Safety-first approach</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default page
